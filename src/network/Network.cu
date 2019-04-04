b735b2f24dc98eb17861fd3fa556c1f2e5fab1f8
Zachary Day <zach@dayzpd.com>
2019-04-04 02:56:55 -0500

    Starting work on back propagation.

--- src/network/Network.cu
+++ src/network/Network.cu
@@ -1,10 +1,16 @@
-#include "LayerFactory.h"
+#include "../layer/LayerFactory.h"
+#include "../loss/LossFactory.h"
+#include "../optimize/OptimizeFactory.h"
 #include "Network.h"
 
 
-Network::Network()
+Network::Network(int num_classes, float learning_rate, std::string loss_type,
+  std::string optimize_type) : num_classes(num_classes),
+  learning_rate(learning_rate)
 {
   this->layer_factory = LayerFactory::get_instance();
+  this->loss_func = LossFactory::get_instance()->create(loss_type);
+  this->optimize_func = OptimizeFactory::get_instance()->create(optimize_type);
 }
 
 Network::Network()
@@ -15,27 +21,55 @@ Network::Network()
   }
 }
 
-Network::add_layer(std::string layer_type)
+void Network::add_feature(Neurons feature, Neurons class)
 {
-  this->layers.push_back(this->layer_factory(layer_type));
+  features.push_back(make_tuple(feature, class));
 }
 
-Network::add_layer(std::string layer_type, size_t x, size_t y)
+void Network::add_layer(std::string layer_type)
 {
-  this->layers.push_back(this->layer_factory(layer_type, Dim(x, y)));
+  this->layers.push_back(this->layer_factory->create(layer_type));
 }
 
-void Network::train(Features& features, int batch_size, float learning_rate)
+void Network::add_layer(std::string layer_type, size_t x, size_t y)
 {
+  this->layers.push_back(this->layer_factory->create(layer_type, Dim(x, y)));
+}
 
+int Network::get_num_classes()
+{
+  return num_classes;
+}
+
+void Network::train()
+{
+  for (auto feature : features)
+  {
+    output = forward_propagate(get<0>(feature));
+    back_propagate(output, get<1>feature);
+  }
 }
 
 Neurons& forward_propagate(Neurons& feature)
 {
+  Neurons layer_output = feature;
+  for (auto layer : layers)
+  {
+    layer_output = layer->forward_prop(layer_output);
+  }
 
+  return layer_output;
 }
 
 void back_propagate(Neurons& prediction, Neurons& actual)
 {
 
+  //TO DO: calculate loss
+  Neurons err = this->loss_func->calculate(prediction, actual);
+
+  for (std::vector<int>::reverse_iterator i = layers.rbegin();
+    i != layers.rend(); i++)
+  {
+    err = *i->back_prop(err, learning_rate);
+  }
 }
--- src/network/network.h
+++ src/network/network.h
@@ -7,6 +7,7 @@
 #include "../optimize/OptimizeFunction.h"
 
 #include <string>
+#include <tuple>
 #include <vector>
 
 namespace neural_network
@@ -16,19 +17,30 @@ namespace neural_network
   {
     private:
       LayerFactory& layer_factory;
+      LossFunction& loss_func;
+      OptimizeFunction& optimize_func;
 
       std::vector<Layer*> layers;
 
+      std::vector<tuple<Neurons, Neurons>> features;
+
+      int num_classes;
+
     public:
-      Network();
+      Network(int num_classes, float learning_rate, std::string loss_type,
+        std::string optimize_type);
 
       ~Network();
+
+      void add_feature(Neurons feature, Neurons class);
 
       // Activation Layer
       void add_layer(std::string layer_type);
 
       // Connection Layer
       void add_layer(std::string layer_type, size_t x, size_t y);
+
+      int get_num_classes();
 
       void train(Features& features, int batch_size, float learning_rate);
 
