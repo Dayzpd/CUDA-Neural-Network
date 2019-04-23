4ffd989d51ee1b4f29a6ac90e4b8fc2a8625f01a
Zachary Day <zach@dayzpd.com>
2019-04-23 00:51:03 -0500

    Slight changes to network header and implementation (moving train method outside Network class)

--- src/network/Network.cu
+++ src/network/Network.cu
@@ -3,108 +3,84 @@
 #include "../optimize/OptimizeFactory.h"
 #include "Network.h"
 
-
-Network::Network(std::string loss_type, std::string optimize_type) :
-  num_batches(0), prob(), prob_delta()
+namespace neural_network
 {
-  this->layer_factory = LayerFactory::get_instance();
-  this->loss_func = LossFactory::get_instance()->create(loss_type);
-  this->optimize_func = OptimizeFactory::get_instance()->create(optimize_type);
-}
 
-Network::~Network()
-{
-  for (auto layer : layers)
+  Network::Network(std::string loss_type) : prob(), prob_delta()
   {
-    delete layer;
+
   }
 
-  for (auto feature : features)
+  Network::~Network()
   {
-    delete feature;
-  }
-}
-
-void Network::add_batch(Neurons features, Neurons classes)
-{
-  batches.push_back(features);
-  actuals.push_back(classes);
-  num_batches += 1;
-}
-
-void Network::add_layer(std::string layer_type)
-{
-  this->layers.push_back(this->layer_factory->create(layer_type));
-}
-
-void Network::add_layer(std::string layer_type, size_t x, size_t y)
-{
-  this->layers.push_back(this->layer_factory->create(layer_type,
-    Dim(x, y)));
-}
-
-std::string classify(Neurons& prediction)
-{
-  prediction->memcpy_device_to_host();
-
-  float max = prediction[0];
-  for (size_t x = 1; x < prediction.dim.x * prediction.dim.y; x++)
-  {
-    if (max < prediction[x])
+    for (auto layer : layers)
     {
-      max = prediction[x];
+      delete layer;
     }
   }
 
-  return this->classes[max];
-}
-
-int Network::get_num_classes()
-{
-  return this->classes.size();
-}
-
-void Network::train(int num_epochs, float learning_rate, int checkpt = 100)
-{
-  for (int e = 0; e < num_epochs; e++)
+  void Network::add_batch(Neurons features, Neurons classes)
   {
-    float loss = 0.0;
-    for (int b = 0; b < num_batches; b++)
+    batches.push_back(features);
+    actuals.push_back(classes);
+    num_batches += 1;
+  }
+
+  void Network::add_layer(std::string layer_type)
+  {
+    layers.push_back(LayerFactory::get_instance().create(layer_type));
+  }
+
+  void Network::add_layer(std::string layer_type, size_t x, size_t y)
+  {
+    layers.push_back(LayerFactory::get_instance().create(layer_type, Dim(x, y)));
+  }
+
+  std::string classify(Neurons& prediction)
+  {
+    prediction->memcpy_device_to_host();
+
+    float max = prediction[0];
+    for (size_t x = 1; x < prediction.dim.x * prediction.dim.y; x++)
     {
-      prob = forward_propagate(batches.at(b));
-      back_propagate(prob, batches.at(b), learning_rate);
-      float loss += this->loss_func.calculate(output, actuals.at(b));
+      if (max < prediction[x])
+      {
+        max = prediction[x];
+      }
     }
 
-    if (e % checkpt == 0)
+    return max;
+  }
+
+  void Network::train(int num_epochs, float learning_rate, int checkpt = 100)
+  {
+
+  }
+
+  Neurons forward_propagate(Neurons& feature)
+  {
+    Neurons output = feature;
+    for (auto layer : layers)
     {
-      printf("Epoch: %d, Cost: %f", e, loss / num_batches);
+      output = layer->forward_prop(layer_output);
+    }
+
+    prob = output;
+
+    return prob;
+  }
+
+  void back_propagate(Neurons& prob, Neurons& actual)
+  {
+    prob_delta.allocate_memory(prob.dim);
+
+    Neurons err = this->loss_func->calculate_deriv(prob, actual, prob_delta);
+
+    for (std::vector<int>::reverse_iterator i = layers.rbegin();
+      i != layers.rend(); i++)
+    {
+      err = *i->back_prop(err, learning_rate);
     }
   }
-}
 
-Neurons forward_propagate(Neurons& feature)
-{
-  Neurons output = feature;
-  for (auto layer : layers)
-  {
-    output = layer->forward_prop(layer_output);
-  }
-
-  prob = output;
-
-  return prob;
-}
-
-void back_propagate(Neurons& prob, Neurons& actual)
-{
-  prob_delta.allocate_memory(prob.dim);
-
-  Neurons err = this->loss_func->calculate_deriv(prob, actual, prob_delta);
-
-  for (std::vector<int>::reverse_iterator i = layers.rbegin();
-    i != layers.rend(); i++)
-  {
-    err = *i->back_prop(err, learning_rate);
-  }
 }
--- src/network/network.h
+++ src/network/network.h
@@ -15,26 +15,15 @@ namespace neural_network
   class Network
   {
     private:
-      LayerFactory& layer_factory;
-      LossFunction& loss_func;
-      OptimizeFunction& optimize_func;
-
       std::vector<Layer*> layers;
 
       Neurons prob;
       Neurons prob_delta;
 
-      std::vector<Neurons> batches;
-      std::vector<Neurons> actuals;
-
-      int num_batches;
-
     public:
-      Network(std::string loss_type, std::string optimize_type);
+      Network();
 
       ~Network();
-
-      void add_batch(Neurons features, Neurons classes);
 
       // Activation Layer
       void add_layer(std::string layer_type);
@@ -43,8 +32,6 @@ namespace neural_network
       void add_layer(std::string layer_type, size_t x, size_t y);
 
       std::string classify(Neurons& prediction);
-
-      int get_num_classes();
 
       void train();
 
